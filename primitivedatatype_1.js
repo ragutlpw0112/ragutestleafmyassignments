757d021efea50d2417d2dc99df39b34474887b89
ragutlpw0112 <s.ragukannan@gmail.com>
2026-09-13 07:17:24 +0530

    Add files via upload
    day1_12sep2026
    primitivedatatype_1

---
+++ primitivedatatype_1.js
@@ -0,0 +1,24 @@
+let firstname = "RAGU";
+let companyname = "company solutions";
+let mobilenumber = 9048058946
+let isautomation = true
+let hasplaywright 
+console.log(firstname)
+console.log("datatype is", typeof(firstname));  
+console.log("\n")
+
+console.log(companyname)
+console.log("datatype is", typeof(companyname))
+console.log("\n")
+
+console.log(mobilenumber)
+console.log("datatype is", typeof(mobilenumber))
+console.log("\n")
+
+console.log(isautomation)
+console.log("datatype is", typeof(isautomation))
+console.log("\n")
+
+console.log(hasplaywright)
+console.log("datatype is", typeof(hasplaywright))
+
